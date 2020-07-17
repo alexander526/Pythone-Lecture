f925e8d0581d1885966c0b1390c62b4224269b91
Alexander Hernandez <alex526.ah@gmail.com>
2020-07-16 22:19:01 -0700

    Add newest files

--- classes.py
+++ classes.py
@@ -1,8 +1,24 @@
-class Point():
-    def __init__(self, input1, input2):
-        self.x = input1
-        self.y = input2
+class Flight():
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.passengers = []
 
-p = Point(2, 8)
-print(p.x)
-print(p.y)
+    def add_passenger(self, name):
+        if not self.open_seats():
+            return False
+        self.passengers.append(name)
+        return True
+
+    def open_seats(self):
+        return self.capacity - len(self.passengers)
+
+flight = Flight(3)
+
+people = ["Harry", "Ron", "Hermione", "Ginny"]
+for person in people:
+    success = flight.add_passenger(person)
+    if success:
+        print(f"Added {person} to flight successfully.")
+    else:
+        print(f"No available seats for {person}")
+        
